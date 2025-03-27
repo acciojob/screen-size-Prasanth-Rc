5efa1bc60c24ec9ae908a5f443c0ce14629ce3d0
Prasanth R <97664738+Prasanth-Rc@users.noreply.github.com>
2025-03-27 12:48:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+ function updateSize() {
+    document.getElementById("width").textContent = window.innerWidth;
+    document.getElementById("height").textContent = window.innerHeight;
+}
+        
+window.addEventListener("resize", updateSize);
+        
+updateSize();
